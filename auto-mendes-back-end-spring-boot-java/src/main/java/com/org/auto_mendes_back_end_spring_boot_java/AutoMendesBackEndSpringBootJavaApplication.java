59c7dcc645b388eaaddf197aedd5b85d9cbdcad1
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2025-01-22 14:27:41 -0300

    commited by Abner

---
+++ auto-mendes-back-end-spring-boot-java/src/main/java/com/org/auto_mendes_back_end_spring_boot_java/AutoMendesBackEndSpringBootJavaApplication.java
@@ -0,0 +1,13 @@
+package com.org.auto_mendes_back_end_spring_boot_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMendesBackEndSpringBootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoMendesBackEndSpringBootJavaApplication.class, args);
+	}
+
+}
---
+++ auto-mendes-back-end-spring-boot-java/src/test/java/com/org/auto_mendes_back_end_spring_boot_java/AutoMendesBackEndSpringBootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.auto_mendes_back_end_spring_boot_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoMendesBackEndSpringBootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
