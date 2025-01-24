31b8b1e0048d42fd61e473e23d5956a02f02eeaf
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2025-01-24 16:21:50 -0300

    commited by Abner

--- auto-mendes-back-end-spring-boot-java/src/main/java/com/org/auto_mendes_back_end_spring_boot_java/entities/Vehicle.java
+++ auto-mendes-back-end-spring-boot-java/src/main/java/com/org/auto_mendes_back_end_spring_boot_java/entities/Vehicle.java
@@ -3,7 +3,7 @@ package com.org.auto_mendes_back_end_spring_boot_java.entities;
 import java.io.Serializable;
 import java.util.List;
 
-import com.org.auto_mendes_back_end_spring_boot_java.enums.GearType;
+import com.org.auto_mendes_back_end_spring_boot_java.enums.ExchangeType;
 import com.org.auto_mendes_back_end_spring_boot_java.enums.VehicleType;
 
 import jakarta.persistence.Column;
@@ -38,7 +38,7 @@ public class Vehicle implements Serializable {
 	@Column(name = "vehicle_type", nullable = false)
 	private VehicleType vehicleType;
 	@Column(name = "gear_type", nullable = false)
-	private GearType gearType;
+	private ExchangeType exchangeType;
 	@ManyToOne
 	@JoinColumn(name = "model_id", nullable = false)
 	private Model model;
--- auto-mendes-back-end-spring-boot-java/src/main/java/com/org/auto_mendes_back_end_spring_boot_java/enums/ExchangeType.java
+++ auto-mendes-back-end-spring-boot-java/src/main/java/com/org/auto_mendes_back_end_spring_boot_java/enums/ExchangeType.java
@@ -1,5 +1,5 @@
 package com.org.auto_mendes_back_end_spring_boot_java.enums;
 
-public enum GearType {
+public enum ExchangeType {
 	Manual, Automatic, CVT
 }
