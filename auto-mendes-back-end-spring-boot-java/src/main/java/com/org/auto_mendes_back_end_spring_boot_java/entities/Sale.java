c82d642158ad9744845eee1afc3c1c813100e77a
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2025-02-05 17:28:57 -0300

    commited by Abner

--- auto-mendes-back-end-spring-boot-java/src/main/java/com/org/auto_mendes_back_end_spring_boot_java/entities/Sale.java
+++ auto-mendes-back-end-spring-boot-java/src/main/java/com/org/auto_mendes_back_end_spring_boot_java/entities/Sale.java
@@ -2,10 +2,15 @@ package com.org.auto_mendes_back_end_spring_boot_java.entities;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -31,7 +36,12 @@ public class Sale implements Serializable {
 	private BigDecimal total;
 	@Column(nullable = false)
 	private String paymentType;
-	private String customer;
-	private String employee;
-	private String saleVehicles;
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Customer customer;
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Employee employee;
+	@OneToMany(mappedBy = "sales", fetch = FetchType.LAZY)
+	private List<SaleVehicle> saleVehicles;
 }
---
+++ auto-mendes-back-end-spring-boot-java/src/main/java/com/org/auto_mendes_back_end_spring_boot_java/entities/SaleVehicle.java
@@ -0,0 +1,24 @@
+package com.org.auto_mendes_back_end_spring_boot_java.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "sales_vehicles")
+public class SaleVehicle implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String id;
+}
