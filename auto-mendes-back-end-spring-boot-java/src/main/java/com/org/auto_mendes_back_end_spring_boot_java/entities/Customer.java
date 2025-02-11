d9ec523f9152cd4369e50f6902e2d7d294fffafd
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2025-02-11 16:40:28 -0300

    commited by Abner

--- auto-mendes-back-end-spring-boot-java/src/main/java/com/org/auto_mendes_back_end_spring_boot_java/entities/Customer.java
+++ auto-mendes-back-end-spring-boot-java/src/main/java/com/org/auto_mendes_back_end_spring_boot_java/entities/Customer.java
@@ -3,12 +3,12 @@ package com.org.auto_mendes_back_end_spring_boot_java.entities;
 import java.io.Serializable;
 import java.util.List;
 
-import com.org.auto_mendes_back_end_spring_boot_java.enums.CustomerType;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -20,6 +20,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 @Table(name = "customers")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Customer implements Serializable {
 	private static final long serialVersionUID = 1L;
 
@@ -29,12 +30,6 @@ public class Customer implements Serializable {
 	private String name;
 	@Column(nullable = false, unique = true)
 	private String telephone;
-	@Column(length = 18)
-	private String cnpj;
-	@Column(length = 14)
-	private String cpf;
-	@Column(nullable = false)
-	private CustomerType customerType;
 	@OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
 	private List<Sale> sales;
 }
