3bcd371aaf265e317ad26165f03dc62b88efafc3
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2025-02-14 18:16:52 -0300

    commited by Abner

--- auto-mendes-back-end-spring-boot-java/src/main/java/com/org/auto_mendes_back_end_spring_boot_java/entities/Saler.java
+++ auto-mendes-back-end-spring-boot-java/src/main/java/com/org/auto_mendes_back_end_spring_boot_java/entities/Saler.java
@@ -10,6 +10,7 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -18,6 +19,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = false)
+@Builder
 @Entity
 @Table(name = "salers")
 @PrimaryKeyJoinColumn(name = "id")
