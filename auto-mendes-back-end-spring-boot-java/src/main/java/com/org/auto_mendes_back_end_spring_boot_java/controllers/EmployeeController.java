64151cf7e3ee222a25e7d4914515ae179df9b85e
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2025-02-19 16:22:17 -0300

    commited by Abner

--- auto-mendes-back-end-spring-boot-java/src/main/java/com/org/auto_mendes_back_end_spring_boot_java/controllers/EmployeeController.java
+++ auto-mendes-back-end-spring-boot-java/src/main/java/com/org/auto_mendes_back_end_spring_boot_java/controllers/EmployeeController.java
@@ -11,21 +11,37 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.org.auto_mendes_back_end_spring_boot_java.dtos.EmployeeRequestDTO;
 import com.org.auto_mendes_back_end_spring_boot_java.dtos.EmployeeResponseDTO;
+import com.org.auto_mendes_back_end_spring_boot_java.dtos.ExceptionResponseDTO;
 import com.org.auto_mendes_back_end_spring_boot_java.enums.EmployeeType;
 import com.org.auto_mendes_back_end_spring_boot_java.services.interfaces.IEmployeeService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/api/employees")
+@Tag(name = "Funcionários", description = "Operações relacionadas aos funcionários")
 public class EmployeeController {
 	@Autowired
 	private IEmployeeService employeeService;
 
+	@Operation(summary = "Registrar funcionário", description = "Registra um funcionário")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "Cria um funcionário", content = @Content(schema = @Schema(implementation = EmployeeResponseDTO.class))),
+			@ApiResponse(responseCode = "400", description = "Retorna um erro de requesição", content = @Content(schema = @Schema(implementation = ExceptionResponseDTO.class))), })
+	@ResponseStatus(value = HttpStatus.CREATED)
 	@PostMapping(value = "/register-employee")
 	public ResponseEntity<EmployeeResponseDTO> registerEmployee(@RequestBody @Valid EmployeeRequestDTO request) {
 		EmployeeResponseDTO employeeResponseDTO = employeeService.registerEmployee(request);
@@ -33,32 +49,52 @@ public class EmployeeController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(employeeResponseDTO);
 	}
 
+	@Operation(summary = "Listar funcionários", description = "Lista funcionários")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Retorna uma pagina de funcionários", content = @Content(schema = @Schema(implementation = Page.class))), })
+	@ResponseStatus(value = HttpStatus.OK)
 	@GetMapping(value = "/list-employees")
-	public ResponseEntity<Page<EmployeeResponseDTO>> listEmployees(Pageable pageable) {
+	public ResponseEntity<Page<EmployeeResponseDTO>> listEmployees(
+			@Parameter(description = "Configura a pagina", required = false) Pageable pageable) {
 		Page<EmployeeResponseDTO> page = employeeService.listEmployees(pageable);
 
 		return ResponseEntity.status(HttpStatus.OK).body(page);
 	}
 
+	@Operation(summary = "Listar funcionários pelo cargo", description = "Lista funcionários pelo cargo")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Retorna uma pagina de funcionários", content = @Content(schema = @Schema(implementation = Page.class))) })
+	@ResponseStatus(value = HttpStatus.OK)
 	@GetMapping(value = "/list-employees-by-position")
-	public ResponseEntity<Page<EmployeeResponseDTO>> listEmployeesByPosition(Pageable pageable,
-			@RequestParam String employeeType) {
+	public ResponseEntity<Page<EmployeeResponseDTO>> listEmployeesByPosition(
+			@Parameter(description = "Configura a pagina", required = false) Pageable pageable,
+			@Parameter(description = "Filtra por cargo", in = ParameterIn.QUERY, required = true, example = "Manager") @RequestParam String employeeType) {
 		Page<EmployeeResponseDTO> page = employeeService.listEmployeesByPosition(pageable,
 				EmployeeType.valueOf(employeeType));
 
 		return ResponseEntity.status(HttpStatus.OK).body(page);
 	}
 
+	@Operation(summary = "Listar funcionários pela matrícula", description = "Lista funcionários pela matrícula")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Retorna uma pagina de funcionários", content = @Content(schema = @Schema(implementation = Page.class))) })
+	@ResponseStatus(value = HttpStatus.OK)
 	@GetMapping(value = "/list-employees-by-matriculation")
-	public ResponseEntity<Page<EmployeeResponseDTO>> listEmployeesByMatriculation(Pageable pageable,
-			@RequestParam String matriculation) {
+	public ResponseEntity<Page<EmployeeResponseDTO>> listEmployeesByMatriculation(
+			@Parameter(description = "Configura a pagina", required = false) Pageable pageable,
+			@Parameter(description = "Filtra por matricula", in = ParameterIn.QUERY, required = true, example = "1") @RequestParam String matriculation) {
 		Page<EmployeeResponseDTO> page = employeeService.listEmployeesByMatriculation(pageable, matriculation);
 
 		return ResponseEntity.status(HttpStatus.OK).body(page);
 	}
 
+	@Operation(summary = "Atualizar funcionário", description = "Atualiza um funcionário pelo id")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Atualiza um funcionário", content = @Content(schema = @Schema(implementation = EmployeeResponseDTO.class))),
+			@ApiResponse(responseCode = "400", description = "Retorna um erro de requesição", content = @Content(schema = @Schema(implementation = ExceptionResponseDTO.class))),
+			@ApiResponse(responseCode = "404", description = "Retorna um erro de não encontrado", content = @Content(schema = @Schema(implementation = ExceptionResponseDTO.class))), })
+	@ResponseStatus(value = HttpStatus.OK)
 	@PutMapping(value = "/update-employee-by-id")
-	public ResponseEntity<EmployeeResponseDTO> updateEmployeeById(@RequestParam String id,
+	public ResponseEntity<EmployeeResponseDTO> updateEmployeeById(@Parameter(description = "Filtra por id", in = ParameterIn.QUERY, required = true, example = "1") @RequestParam String id,
 			@RequestBody @Valid EmployeeRequestDTO request) {
 		EmployeeResponseDTO employeeResponseDTO = employeeService.updateEmployeeById(id, request);
 
--- auto-mendes-back-end-spring-boot-java/src/main/java/com/org/auto_mendes_back_end_spring_boot_java/dtos/EmployeeResponseDTO.java
+++ auto-mendes-back-end-spring-boot-java/src/main/java/com/org/auto_mendes_back_end_spring_boot_java/dtos/EmployeeResponseDTO.java
@@ -6,21 +6,31 @@ import com.org.auto_mendes_back_end_spring_boot_java.entities.DeputyManager;
 import com.org.auto_mendes_back_end_spring_boot_java.entities.Manager;
 import com.org.auto_mendes_back_end_spring_boot_java.entities.Saler;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "DTO de resposta do funcionário")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmployeeResponseDTO {
+	@Schema(description = "ID do funcionario")
 	private String id;
+	@Schema(description = "Nome do funcionario")
 	private String name;
+	@Schema(description = "CPF do funcionario")
 	private String cpf;
+	@Schema(description = "Email do funcionario")
 	private String email;
+	@Schema(description = "Telefone do funcionario")
 	private String telephone;
+	@Schema(description = "Salário do funcionario")
 	private BigDecimal salary;
+	@Schema(description = "Matrícula do funcionario")
 	private String matriculation;
+	@Schema(description = "Comissão do funcionario")
 	private BigDecimal commission;
 	
 	public EmployeeResponseDTO(Manager manager) {
