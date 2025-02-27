22a749bdf76e228c66e141134ff48e0c4f4d8f4c
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2025-02-27 10:37:19 -0300

    commited by Abner

--- backend/src/main/java/com/org/auto_mendes_back_end_spring_boot_java/validations/VehicleValidation.java
+++ backend/src/main/java/com/org/auto_mendes_back_end_spring_boot_java/validations/VehicleValidation.java
@@ -1,10 +1,31 @@
 package com.org.auto_mendes_back_end_spring_boot_java.validations;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.org.auto_mendes_back_end_spring_boot_java.entities.Car;
+import com.org.auto_mendes_back_end_spring_boot_java.entities.Motorcycle;
+import com.org.auto_mendes_back_end_spring_boot_java.exceptions.ValidationException;
+import com.org.auto_mendes_back_end_spring_boot_java.repositories.interfaces.IModelRepository;
 import com.org.auto_mendes_back_end_spring_boot_java.validations.interfaces.IVehicleValidation;
 
 @Component
 public class VehicleValidation implements IVehicleValidation {
-	
+	@Autowired
+	private IModelRepository modelRepository;
+
+	public void validateCar(Car car) {
+		boolean isExistsModelName = modelRepository.existsByName(car.getModel().getName());
+
+		if (!isExistsModelName)
+			throw new ValidationException("Modelo ");
+
+	}
+
+	public void validateMotorcycle(Motorcycle motorcycle) {
+		boolean isExistsModelName = modelRepository.existsByName(motorcycle.getModel().getName());
+
+		if (isExistsModelName)
+			throw new ValidationException();
+	}
 }
