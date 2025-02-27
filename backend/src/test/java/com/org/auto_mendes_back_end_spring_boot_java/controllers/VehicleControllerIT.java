f756e166efbdbbfaebba9c0f245bf4dc56fa999c
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2025-02-26 21:47:41 -0300

    commited by Abner

--- backend/src/test/java/com/org/auto_mendes_back_end_spring_boot_java/controllers/VehicleControllerIT.java
+++ backend/src/test/java/com/org/auto_mendes_back_end_spring_boot_java/controllers/VehicleControllerIT.java
@@ -18,8 +18,12 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.org.auto_mendes_back_end_spring_boot_java.dtos.requests.VehicleRequestDTO;
+import com.org.auto_mendes_back_end_spring_boot_java.entities.Car;
+import com.org.auto_mendes_back_end_spring_boot_java.entities.Model;
+import com.org.auto_mendes_back_end_spring_boot_java.entities.Motorcycle;
 import com.org.auto_mendes_back_end_spring_boot_java.enums.ExchangeType;
 import com.org.auto_mendes_back_end_spring_boot_java.enums.VehicleType;
+import com.org.auto_mendes_back_end_spring_boot_java.repositories.interfaces.IModelRepository;
 import com.org.auto_mendes_back_end_spring_boot_java.repositories.interfaces.IVehicleRepository;
 
 @SpringBootTest
@@ -33,7 +37,15 @@ class VehicleControllerIT {
 	@Autowired
 	private IVehicleRepository vehicleRepository;
 	@Autowired
+	private IModelRepository modelRepository;
+	@Autowired
 	private VehicleRequestDTO dto;
+	@Autowired
+	private Car car;
+	@Autowired
+	private Motorcycle motorcycle;
+	@Autowired
+	private Model model;
 	
 	@BeforeEach
 	void setUp() {
@@ -42,6 +54,7 @@ class VehicleControllerIT {
 	@AfterEach
 	void tearDown() {
 		vehicleRepository.deleteAll();
+		modelRepository.deleteAll();
 	}
 	
 	@Test
@@ -57,5 +70,32 @@ class VehicleControllerIT {
 
 		mockMvc.perform(post("/api/vehicles/register-vehicle").contentType(MediaType.APPLICATION_JSON)
 				.accept(MediaType.APPLICATION_JSON).content(json)).andExpect(status().isCreated()).andDo(print());
+	}
+	
+	
+	void loadVehicles() {
+		model.setId(null);
+		model.setMark(null);
+		model.setName(null);
+		
+		modelRepository.save(model);
+		
+		car.setColor(null);
+		car.setExchangeType(null);
+		car.setId(null);
+		car.setModel(null);
+		car.setVehicleValue(new BigDecimal("2500.00"));
+		car.setVehicleYear("2010");
+		
+		vehicleRepository.save(car);
+		
+		motorcycle.setColor(null);
+		motorcycle.setExchangeType(null);
+		motorcycle.setId(null);
+		motorcycle.setModel(null);
+		motorcycle.setVehicleValue(new BigDecimal("2500.00"));
+		motorcycle.setVehicleYear("2010");
+		
+		vehicleRepository.save(motorcycle);
 	}
 }
