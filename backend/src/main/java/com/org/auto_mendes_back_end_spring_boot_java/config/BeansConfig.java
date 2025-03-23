a37652f3c8239a8b56b84fb69beb1a234272777c
ABNER JOSÉ FELIX BARBOSA <abnerbarbosa95@gmail.com>
2025-03-23 18:06:55 -0300

    Delete backend directory

--- backend/src/main/java/com/org/auto_mendes_back_end_spring_boot_java/config/BeansConfig.java
+++
@@ -1,8 +0,0 @@
-package com.org.auto_mendes_back_end_spring_boot_java.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class BeansConfig {
-	
-}
--- backend/src/main/java/com/org/auto_mendes_back_end_spring_boot_java/validations/interfaces/IMarkValidation.java
+++
@@ -1,7 +0,0 @@
-package com.org.auto_mendes_back_end_spring_boot_java.validations.interfaces;
-
-import com.org.auto_mendes_back_end_spring_boot_java.entities.Mark;
-
-public interface IMarkValidation {
-	void validateMark(Mark mark);
-}
