6b8f15c94a5cb687692851da8bdf308a7afe1e19
ABNER JOSÉ FELIX BARBOSA <abnerbarbosa95@gmail.com>
2025-03-23 16:48:23 -0300

    Delete backend directory

--- backend/src/main/java/com/org/auto_mendes_back_end_spring_boot_java/validations/interfaces/IVehicleValidation.java
+++
@@ -1,9 +0,0 @@
-package com.org.auto_mendes_back_end_spring_boot_java.validations.interfaces;
-
-import com.org.auto_mendes_back_end_spring_boot_java.entities.Car;
-import com.org.auto_mendes_back_end_spring_boot_java.entities.Motorcycle;
-
-public interface IVehicleValidation {
-	void validateCar(Car car);
-	void validateMotorcycle(Motorcycle motorcycle);
-}
