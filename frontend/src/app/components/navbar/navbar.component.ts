2ddf043e51583cb90d98fbe7163cccd34827225f
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2025-04-12 18:01:20 -0300

    Atualizando navbar

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-navbar',
-  imports: [ButtonModule],
+  imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
   standalone: true
