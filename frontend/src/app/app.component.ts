568e3f179cf9a3eed27001cb85601807987b4c5f
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2025-04-15 15:16:33 -0300

    Comentado por Abner

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/page/employee-registration/employee-registration.component.ts
+++ frontend/src/app/page/employee-registration/employee-registration.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-registration',
   imports: [NavbarComponent],
   templateUrl: './employee-registration.component.html',
-  styleUrl: './employee-registration.component.css'
+  styleUrl: './employee-registration.component.css',
 })
 export class EmployeeRegistrationComponent {
+  fg!: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.fg = this.fb.group({
+      name: ['', [Validators.required, Validators.maxLength(100)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.fg.valid) {
+      console.log(this.fg.value);
+    } else {
+      this.fg.markAllAsTouched();
+    }
+  }
 }
