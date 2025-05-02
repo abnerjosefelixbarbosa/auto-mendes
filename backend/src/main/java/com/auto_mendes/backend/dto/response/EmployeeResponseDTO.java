687517fb722ef3f92d5915764463a58487661baf
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2025-05-02 14:31:51 -0300

    Comitado por Abner.

--- backend/src/main/java/com/auto_mendes/backend/dto/response/EmployeeResponseDTO.java
+++ backend/src/main/java/com/auto_mendes/backend/dto/response/EmployeeResponseDTO.java
@@ -3,6 +3,8 @@ package com.auto_mendes.backend.dto.response;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import com.auto_mendes.backend.enums.EmployeeType;
+
 public record EmployeeResponseDTO(
 		String id,
 		String name,
@@ -10,5 +12,6 @@ public record EmployeeResponseDTO(
 		String matriculation, 
 		String phone,
 		LocalDate birthDate,
+		EmployeeType employeeType,
 		BigDecimal commission
 ) {}
--- backend/src/main/java/com/auto_mendes/backend/mapper/EmployeeMapper.java
+++ backend/src/main/java/com/auto_mendes/backend/mapper/EmployeeMapper.java
@@ -2,6 +2,7 @@ package com.auto_mendes.backend.mapper;
 
 import com.auto_mendes.backend.dto.request.EmployeeRequestDTO;
 import com.auto_mendes.backend.dto.response.EmployeeResponseDTO;
+import com.auto_mendes.backend.enums.EmployeeType;
 import com.auto_mendes.backend.model.AssistantManager;
 import com.auto_mendes.backend.model.Manager;
 import com.auto_mendes.backend.model.Saler;
@@ -22,16 +23,16 @@ public class EmployeeMapper {
 
 	public static EmployeeResponseDTO toEmployeeResponseDTO(Manager entity) {
 		return new EmployeeResponseDTO(entity.getId(), entity.getName(), entity.getEmail(), entity.getMatriculation(),
-				entity.getPhone(), entity.getBirthDate(), null);
+				entity.getPhone(), entity.getBirthDate(), EmployeeType.MANAGER, null);
 	}
 
 	public static EmployeeResponseDTO toEmployeeResponseDTO(AssistantManager entity) {
 		return new EmployeeResponseDTO(entity.getId(), entity.getName(), entity.getEmail(), entity.getMatriculation(),
-				entity.getPhone(), entity.getBirthDate(), null);
+				entity.getPhone(), entity.getBirthDate(), EmployeeType.ASSISTANT_MANAGER, null);
 	}
 
 	public static EmployeeResponseDTO toEmployeeResponseDTO(Saler entity) {
 		return new EmployeeResponseDTO(entity.getId(), entity.getName(), entity.getEmail(), entity.getMatriculation(),
-				entity.getPhone(), entity.getBirthDate(), entity.getCommission());
+				entity.getPhone(), entity.getBirthDate(), EmployeeType.SALER, entity.getCommission());
 	}
 }
