1709834dc3fdb982f48d5e448e1ba2434a61cc80
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2025-05-31 14:00:20 -0300

    Comitado por Abner.

--- backend/src/main/java/com/auto_mendes/backend/controller/EmployeeController.java
+++ backend/src/main/java/com/auto_mendes/backend/controller/EmployeeController.java
@@ -12,12 +12,16 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.auto_mendes.backend.dto.EmployeeRequestDTO;
 import com.auto_mendes.backend.dto.EmployeeResponseDTO;
 import com.auto_mendes.backend.service.IEmployeeService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
 @RestController
@@ -26,6 +30,10 @@ public class EmployeeController {
 	@Autowired
 	private IEmployeeService employeeService;
 
+	@ApiResponses(value = { @ApiResponse(responseCode = "201", description = "Cria um funcionário."),
+			@ApiResponse(responseCode = "400", description = "Retorna um erro na requesição.") })
+	@ResponseStatus(value = HttpStatus.CREATED)
+	@Operation(summary = "Registra funcionário.", description = "Registra um funcionário.")
 	@PostMapping(value = "/register-employee", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<EmployeeResponseDTO> registerEmployee(@RequestBody @Valid EmployeeRequestDTO dto) {
 		EmployeeResponseDTO response = employeeService.registerEmployee(dto);
@@ -33,6 +41,11 @@ public class EmployeeController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Atualiza um funcionário."),
+			@ApiResponse(responseCode = "400", description = "Retorna um erro na requesição."),
+			@ApiResponse(responseCode = "404", description = "Retorna funcionário não encontrado.") })
+	@ResponseStatus(value = HttpStatus.OK)
+	@Operation(summary = "Atualiza funcionário pelo id.", description = "Atualiza um funcionário pelo id.")
 	@PutMapping(value = "/update-employee-id", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<EmployeeResponseDTO> updateEmployeeById(@RequestParam String id,
 			@RequestBody @Valid EmployeeRequestDTO dto) {
@@ -41,6 +54,9 @@ public class EmployeeController {
 		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Retorna todos os funcionários.") })
+	@ResponseStatus(value = HttpStatus.OK)
+	@Operation(summary = "Lista funcionários.", description = "Lista todos os funcionário.")
 	@GetMapping(value = "/list-employees", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Page<EmployeeResponseDTO>> listEmployees(Pageable pageable) {
 		Page<EmployeeResponseDTO> page = employeeService.listEmployees(pageable);
